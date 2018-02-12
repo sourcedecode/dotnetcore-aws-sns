d0cebae4c365f97541397b33e01fb1bb657ed45c
praveendhatric <praveendhatric@MacBookPro.local>
2018-02-12 09:51:22 -0500

    Template Project

---
+++ aws-sns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aws_sns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
